d6f0387bbcb731206421494d20301f5cbd8bdfb9
Redzhiyao <iamjovantan@gmail.com>
2024-05-12 23:08:30 +0800

    Week04 Practical01 Completed

--- myApp/Week04/books-api-mvc-db/middlewares/validateBook.js
+++ myApp/Week04/books-api-mvc-db/middlewares/validateBook.js
@@ -1,4 +1,4 @@
-const joi = require("joi");
+const Joi = require("joi");
 
 const validateBook = (req, res, next) => {
   const schema = Joi.object({
@@ -6,15 +6,15 @@ const validateBook = (req, res, next) => {
     author: Joi.string().min(3).max(50).required(),
   });
 
-  const validation = schema.validate(req.body, { abortEarly: false });
+  const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
 
   if (validation.error) {
     const errors = validation.error.details.map((error) => error.message);
     res.status(400).json({ message: "Validation error", errors });
-    return;
+    return; // Terminate middleware execution on validation error
   }
 
-  next();
-};  
+  next(); // If validation passes, proceed to the next route handler
+};
 
-module.exports = validateBook;
+module.exports = validateBook;
